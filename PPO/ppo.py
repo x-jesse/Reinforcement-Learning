906302b47699c9c3394792520b382aa7c1f5398d
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-06-17 20:12:40 -0400

    Update docs

--- PPO/ppo.py
+++ PPO/ppo.py
@@ -1,2 +1,11 @@
-# implement ppo:
-# - 
+import gymnasium as gym
+
+import torch
+import torch.nn as nn
+
+env = gym.make('MountainCarContinuous-v0', render_mode = 'human')
+state, info = env.reset()
+
+
+
+
