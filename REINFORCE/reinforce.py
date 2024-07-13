4144484f3c82fd96bb0c865e46ee1360e4890347
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-07-12 21:23:01 -0400

    Update docs

--- REINFORCE/reinforce.py
+++ REINFORCE/reinforce.py
@@ -15,22 +15,23 @@
     require a bit of math to fully understand. 
 
     This section of the guide will serve as a deep-dive into the concepts and driving principles 
-    behind the REINFORCE algorithm. 
+    behind the REINFORCE algorithm. (Based on the pseudo-code provided by the book 
+    "Reinforcement Learning: An Introduction" by Richard S. Sutton and Andrew G. Barto) 
 
-    REINFORCE, or Monte Carlo policy gradient, is a RL method based on gradient ascent. We want
-    to optimize our policy in the direction of maximal reward. The general process is:
+    REINFORCE, or Monte Carlo policy gradient, is a Monte Carlo method based on stochastic gradient 
+    ascent. We want to optimize our policy in the direction of maximal reward. The general process is:
 
         1. Run an epoch, recording information as we go. This includes states, actions, and rewards.
 
-        2. After termination, we update our policy using *Policy Gradient Theorem*.
+        2. After termination, we update our policy using Policy Gradient Theorem.
 
         3. Repeat until convergence.
 
     Additional notes:
     - REINFORCE should not be confused with VPG (vanilla policy gradient). Though similar, they
     are slightly different in their calculation of return.
-    - This algorithm is the direct implmementation of the pseudo-code from "Intro. to RL" by 
-    Richard S. Sutton and Andrew G. Barto. Feel free to check out that book for additional
+    - "Reinforcement Learning: An Introduction" by Richard S. Sutton and Andrew G. Barto can be found here:
+    http://incompleteideas.net/book/the-book-2nd.html. Feel free to check it out for additional
     explanations!
 
     Please note: this section does assume you have a basic understanding of Python and Pytorch
@@ -39,7 +40,6 @@
     Happy learning! <3
 
 """
-
 import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np
@@ -62,7 +62,6 @@ You can find detailed documentation here:
 https://gymnasium.farama.org/environments/classic_control/cart_pole/
 
 """
-
 # This is a standard feed-forward neural network :)
 # Optional reading: https://en.wikipedia.org/wiki/Feedforward_neural_network
 class FeedForward(nn.Module):
@@ -74,7 +73,6 @@ class FeedForward(nn.Module):
     be able to achieve the maximum reward of 500.
 
     """
-
     def __init__(self, n_obs, n_actions):
         super().__init__()
         # the number of nodes per layer is pretty arbitrary
@@ -124,26 +122,46 @@ for n_epoch in range(max_eps):
     go through it in order, starting from policy initialization, to return calculation,
     and finally gradient updating via backpropagation.
 
-    If you're not familiar with machine learning, feel free to skip this next paragraph.
+    --Aside--
+
+    If you're not familiar with machine learning, feel free to skip this next section.
     If you are, I'd like to draw some connections between training neural networks in RL vs
-    other types of ML. There are many ways to train a neural network to perform certain tasks,
-    and RL does take advantage of that by using neural networks as a policy. However, unlike
-    some other types of ML, such as supervised learning, the loss calculation is not well-defined.
+    other types of ML. Generally speaking, neural networks are not explicitly referenced in
+    most RL literature, since they usually are not the focus of most algorithms. However, policies 
+    in RL are almost always implemented using neural networks, which then begs the question - 
+    how is RL any different from other types of machine learning, eg. unsupervised learning?
+
+    While neural networks are oftentimes a shared similarity between different types of machine learning,
+    the difference lies in the ways that they are trained. Neural networks are very general purpose
+    function approximators, which lends themselves well to many different applications. NLP, image processing,
+    search & recommendation systems can all apply neural networks in a variety of ways. By changing the way
+    we define our loss (error) function, we can easily pivot from one type of machine learning to another.
+
+    A reinforcement learning problem can just as easily become a supervised learning one by simply structuring
+    our data and substituting our loss function, and vice versa. It's up to the engineer to decide what method
+    is most applicable, and when - that goes beyond the scope of this guide though :P .
+    --------
     
-    In RL, we need to define our own metrics - and this is the challenge that many RL algorithms
-    try to tackle, by providing different ways for us to update our policies. 
+    There are many ways to train a neural network to perform certain tasks, and RL does take advantage of that 
+    by using neural networks as a policy. However, unlike some other types of ML, such as supervised learning, the 
+    loss calculation is not as well-defined. In RL, we need to define our own metrics - and this is the challenge 
+    that many RL algorithms try to tackle by providing different ways for us to update our policies. 
     
     """
-    cum_reward = 0
+    cum_reward = 0 # we obv can't use the word return in python, so we'll call it cumulative reward instead
     states, actions, rewards = [], [], []
     saved_log_probs = []
     state, info = env.reset()
 
     # loop until our episode terminates
     while True:
+        # convert state to tensor
+        state = torch.tensor(state, device=device)
+
         # we need to wrap our state in an extra dimension,
         # otherwise PyTorch gets angry when we try to calculate loss later
-        state = torch.tensor(state, device=device).unsqueeze(0)
+        state = state.unsqueeze(0)
+
         # gets the logit outputs from our policy
         logits = policy(state)
 
@@ -183,10 +201,10 @@ for n_epoch in range(max_eps):
     We want to compute our discounted reward here. Recall that for environments where
     we can potentially have very high timesteps, we want our reward series to remain finite.
     It would a problem for us if our return approached infinity, so we introduce a discount
-    factor. Strictly speaking, OpenAI Gymnasium has a built in condition that prevents episodes
+    factor: gamma. Strictly speaking, OpenAI Gymnasium has a built-in condition that prevents episodes
     from exceeding a certain length, so infinite-timestep episodes aren't really a concern. 
     Empirically though, introducing a discount factor is beneficial to assist convergence
-    and decrease variance in our gradient computation, so there's no downside to including it.
+    and decrease variance in our return, so there's no downside to including it.
     
     Our discounted return calculation takes the form of a repeated sum, looped over the reversed
     rewards array:
@@ -194,12 +212,15 @@ for n_epoch in range(max_eps):
         for r in rewards[::-1]:
             discounted_reward = r + gamma * discounted_reward
 
-    This might seem weird at first, since discounting happens in the forward direction - we
+    This might seem weird at first, since discounting reward happens in the forward direction - we
     expect future values to have a lower value than current ones. However, this calculation is
     for return, and not reward. The variable itself represents the discounted reward for some given
-    timestep up to the end state. In other words, it is a summation of values from there to the end.
-    """
+    timestep up to the end state. In other words, it is a summation of values from that state up until
+    episode termination. 
 
+
+
+    """
     for r in rewards[::-1]:
         # computes the discounted reward at that timestep
         # assuming we have rewards = [1, 1, 1, 1]
