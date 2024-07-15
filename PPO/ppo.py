81a08639ad3421b5042a209711f6282897613669
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2024-07-15 17:45:15 -0400

    Add trajectory sampling

--- PPO/ppo.py
+++ PPO/ppo.py
@@ -14,7 +14,7 @@ class FeedForward(nn.Module):
     a way for us to sample actions. Initially, all the weights will be random;
     our actions will be nonsensical and arbitrary. But as we learn, our policy
     will eventually converge to the optimal one, and our agent should consistently 
-    be able to achieve the maximum reward of 500.
+    be able to achieve the maximum reward.
 
     """
     def __init__(self, n_obs, n_actions):
@@ -37,16 +37,25 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-hu', '--human', action='store_true')
 args = parser.parse_args()
 
+# we can set our model to run on GPU or Apple Silicon if it's available
+# better hardware = faster training
+device = "cpu"
+# if torch.cuda.is_available():
+#     device = "cuda"
+# elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+#     device = "mps"
+
 env = gym.make('Pendulum-v1', render_mode='human' if args.human else None)
 state, _ = env.reset()
 print(env.observation_space.shape, env.action_space.shape)
 act_dim = env.action_space.shape[0]
 obs_dim = env.observation_space.shape[0]
 actor = FeedForward(obs_dim, act_dim)
-
+actor.to(device)
 critic = FeedForward(obs_dim, act_dim)
+critic.to(device)
 
-covar = torch.diag(torch.full(size=env.action_space.shape, fill_value=0.5))
+covar = torch.diag(torch.full(size=env.action_space.shape, fill_value=0.5, device=device))
 # print(covar)
 
 # gather trajectories
@@ -58,14 +67,32 @@ covar = torch.diag(torch.full(size=env.action_space.shape, fill_value=0.5))
 
 """
 max_timesteps = 200
-for tstep in range(max_timesteps):
-    state = torch.tensor(state)
-    distr = MultivariateNormal(actor(state), covar)
-    action = distr.sample()
+batch_size = 600
 
-    new_state, reward, terminated, truncated, _ = env.step(action.numpy())
+states, rewards = [], []
+saved_log_probs = []
+k = 0
 
-    if terminated or truncated:
-        break
+# sample some trajectories
+while k < batch_size:
+    # step through the episode
+    for tstep in range(max_timesteps):
+        state = torch.tensor(state, device=device)
+        states.append(state)
+        # normal distribution for action sampling
+        distr = MultivariateNormal(actor(state), covar)
+        action = distr.sample()
+        saved_log_probs.append(distr.log_prob(action))
 
-    state = new_state
+        # take a step in our env
+        new_state, reward, terminated, truncated, _ = env.step(action.numpy())
+        rewards.append(reward)
+        # print(state, action, reward)
+        # increment counter
+        k += 1
+
+        if terminated or truncated:
+            break
+        state = new_state
+
+print(states, saved_log_probs, rewards)
